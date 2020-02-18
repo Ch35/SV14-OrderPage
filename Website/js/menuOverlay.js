da6c2d257fb05b2ccc7b3b29a74fbde01485a41e
Ch35 <ryangray1111@gmail.com>
2020-02-18 21:26:18 +0200

    Added partial functionality to the order page.

--- Website/js/menuOverlay.js
+++ Website/js/menuOverlay.js
@@ -28,11 +28,38 @@ function closeNav(overlayClass){
 
 
 // Accept T&Cs
-var arParts = [];
+var arrParts = [];
 function AcceptTerms(){
+  var spinnaker = document.getElementsByClassName("Spinnaker")[0];
+  var rSpinnaker = document.getElementsByClassName("Racing-Spinnaker")[0];
+  var rJib = document.getElementsByClassName("Racing-Jib")[0];
+  var rSail = document.getElementsByClassName("Racing-MainSail")[0];
+  var seats = document.getElementsByClassName("Canting-Seats")[0];
+  var input = document.getElementsByTagName("input");
+
   document.getElementById("orderTitle").innerHTML = "Order";
   document.getElementsByClassName("Terms-Content")[0].style.display = "none";
   document.getElementsByClassName("Order-Content")[0].style.display = "block";
+
+  if(spinnaker.style.opacity == "1"){
+    input[1].value = "1";
+  } else {input[1].value = "0"}
+
+  if(rSpinnaker.style.opacity == "1"){
+    input[5].value = "1";
+  } else {input[5].value = "0"}
+
+  if(rJib.style.opacity == "1"){
+    input[4].value = "1";
+  } else {input[4].value = "0"}
+
+  if(rSail.style.opacity == "1"){
+    input[3].value = "1";
+  } else {input[3].value = "0"}
+
+  if(seats.style.opacity == "1"){
+    input[2].value = "1";
+  } else {input[2].value = "0"}
 }
 
 
